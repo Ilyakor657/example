943828cd5dd25ee168b1b2f1c5d0d97900097efe
Ilya Korolev <82649037+Ilyakor657@users.noreply.github.com>
2023-10-16 17:28:09 +0400

    commit

---
+++ mysql/index.php
@@ -0,0 +1,31 @@
+<?php
+  class DataBase {
+    private static $instance;
+    private $connection;
+
+    private $host = 'localhost';
+    private $port = '3306';
+    private $dbname = 'test';
+    private $user = 'root';
+    private $password = '';
+
+    private function __construct() {
+      $this->connection = new mysqli($this->host, $this->user, $this->password, $this->dbname, $this->port);
+      if ($this->connection->connect_error) {
+        die("Ошибка подключения: " . $this->connection->connect_error);
+      }
+      echo $this->connection->host_info;
+    }
+
+    public static function getInstance(){
+      if (self::$instance === null) {
+          self::$instance = new DataBase();
+      }
+      return self::$instance;
+    }
+
+    private function __clone() {}
+  }
+
+  Database::getInstance();
+?>
