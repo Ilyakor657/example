61f60072da93501e4c14fddd6a624cb621d75a6a
Ilyakor657 <korolevila15@gmail.com>
2023-10-31 14:22:31 +0400

    singleton

--- mysql-singleton/index.php
+++ mysql-singleton/index.php
@@ -1,31 +1,28 @@
 <?php
-  class DataBase {
-    private static $instance;
-    private $connection;
+class DataBase {
+  private static $instance;
+  private $connection;
 
-    private $host = 'localhost';
-    private $port = '3306';
-    private $dbname = 'test';
-    private $user = 'root';
-    private $password = '';
+  private $host = 'localhost';
+  private $port = '3306';
+  private $dbname = 'test';
+  private $user = 'root';
+  private $password = '';
 
-    private function __construct() {
-      $this->connection = new mysqli($this->host, $this->user, $this->password, $this->dbname, $this->port);
-      if ($this->connection->connect_error) {
-        die("Ошибка подключения: " . $this->connection->connect_error);
-      }
-      echo $this->connection->host_info;
+  private function __construct() {
+    $this->connection = new mysqli($this->host, $this->user, $this->password, $this->dbname, $this->port);
+    if ($this->connection->connect_error) {
+      die("Ошибка подключения: " . $this->connection->connect_error);
     }
-
-    public static function getInstance(){
-      if (self::$instance === null) {
-        self::$instance = new DataBase();
-      }
-      return self::$instance;
-    }
-
-    private function __clone() {}
+    echo $this->connection->host_info;
   }
 
-  $db = Database::getInstance();
-?>
+  public static function getInstance(){
+    if (self::$instance === null) {
+      self::$instance = new DataBase();
+    }
+    return self::$instance;
+  }
+}
+
+$db = Database::getInstance();
